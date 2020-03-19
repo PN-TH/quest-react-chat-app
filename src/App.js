20514289fc89a885053dee68edaf9138601da046
Thomas <tponthoreau@gmail.com>
2020-03-19 15:38:07 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Contact from './components/Contact'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+        name="Carmen Armstrong"
+        avatar="https://randomuser.me/api/portraits/women/26.jpg"
+        status={true} 
+        />
+        <Contact 
+        name="Victoria Tucker"
+        avatar="https://randomuser.me/api/portraits/women/94.jpg"
+        status={false}
+        />
+         <Contact 
+        name="Camila Guterriez"
+        avatar="https://randomuser.me/api/portraits/women/24.jpg"
+        status={false}
+        />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,44 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
-const user = {
-    name: 'Carmen Armstrong',
-    avatar: 'https://randomuser.me/api/portraits/women/26.jpg',
-    status: true
-}
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    status: PropTypes.bool.isRequired
+};
 
-function isConnected(status){
-    if (status) {
-        return (
-        <div className="status">
-            <div class="status-online"></div> 
-            <p class="status-text">online</p>
-        </div>
-        );
-    } else {
-        return (
+function Contact(props){
+
+    function isConnected(){
+        if (props.status) {
+            return (
             <div className="status">
-                <div class="status-offline"></div> 
-                <p class="status-text">offline</p>
+                <div class="status-online"></div> 
+                <p class="status-text">online</p>
             </div>
-        );
+            );
+        } else {
+            return (
+                <div className="status">
+                    <div class="status-offline"></div> 
+                    <p class="status-text">offline</p>
+                </div>
+            );
+        }
     }
-}
 
-function Contact(){
 return (
     <div className="Contact">
-        <img className="avatar" src={user.avatar} alt={user.name} />
+        <img className="avatar" src={props.avatar} alt={props.name} />
         <div>
-            <h4 className="name">{user.name}</h4>
-            <div className="status">
-                {isConnected(user.status)}
-            </div>
+            <h4 className="name">{props.name}</h4>
+                {isConnected(props.status)}
         </div>
     </div>
     );
 }
+
+
 
 export default Contact;
